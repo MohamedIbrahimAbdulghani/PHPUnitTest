0ae904601814216dcdb88bb6e0240a6d21edea75
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-08-06 17:23:36 +0300

    use function setUp() function

--- Crud-Database/tests/userTest.php
+++ Crud-Database/tests/userTest.php
@@ -5,8 +5,15 @@ require 'database/user.php';
 use PHPUnit\Framework\TestCase;
 
 class userTest extends TestCase {
+    private $object;
+    // this function to don't repeated object in a new functions write ( $this->object ) don't write $user = new user(); in any function
+    /*
+        *-* Don't Try Yourself  ( DRY )
+    */
+    protected function setUp():void {
+        $this->object = new user();
+    }
     public function test_insert_new_user() {
-        $user = new user();
-        $this->assertTrue($user->add('test email', 'test password'));
+        $this->assertTrue($this->object->add('test email', 'test password'));
     }
 }
