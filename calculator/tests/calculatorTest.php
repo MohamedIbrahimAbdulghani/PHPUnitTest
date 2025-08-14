fcbdd3852eb0b480b90f426b104531ac6ef0c470
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-08-14 13:13:24 +0300

    use annotations group

--- calculator/tests/calculatorTest.php
+++ calculator/tests/calculatorTest.php
@@ -11,22 +11,39 @@ class calculatorTest extends TestCase {
         $this->myObject = new calculator();
     }
     // this function to make test about add function in calculator
+    
+    // use Annotations Group
+    /**
+     * @group baseFunction
+     */
     public function test_assert_add_calc() {
         $this->assertEquals($this->myObject->add(10, 90), 100);
     }
     // this function to make test about sub function in calculator
+    /**
+     * @group baseFunction
+     */
     public function test_assert_sub_calc() {
         $this->assertEquals($this->myObject->sub(100, 90), 10);
     }
     // this function to make test about multi function in calculator
+    /**
+     * @group baseFunction
+     */
     public function test_assert_multi_calc() {
         $this->assertEquals($this->myObject->multi(10, 10), 100);
     }
     // this function to make test about div function in calculator
+    /**
+     * @group baseFunction
+     */
     public function test_assert_div_calc() {
         $this->assertEquals($this->myObject->div(100, 10), 10);
     }
     // this function to make test about models function in calculator
+    /**
+     * @group advanceFunction
+     */
     public function test_assert_models_calc() {
         $this->assertEquals($this->myObject->models(11, 2), 1);
     }
